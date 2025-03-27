0cf847f40173eb21bd963ce74fb58e808330a840
NH <mnh4140@gmail.com>
2025-03-27 16:20:29 +0900

    env: 코드베이스 개발 환경 셋팅
    - 스토리보드 설정 삭제

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -6,14 +6,30 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+    
+    let label = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configureUI()
     }
-
-
+    
+    func configureUI() {
+        label.text = "Hello World"
+        label.textColor = .white
+        label.textAlignment = .center
+        label.font = .boldSystemFont(ofSize: 20)
+        
+        view.addSubview(label)
+        
+        label.snp.makeConstraints {
+            $0.width.height.equalTo(200)
+            $0.center.equalToSuperview()
+        }
+    }
 }
 
